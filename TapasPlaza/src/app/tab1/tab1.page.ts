4b5eae3a48942e1d35f360daed83000a023fd4aa
Emilia Rosa van der Heide <emvader@epsg.upv.es>
2020-04-08 16:52:16 +0200

    automatic slider carrousel images

--- TapasPlaza/src/app/tab1/tab1.page.ts
+++ TapasPlaza/src/app/tab1/tab1.page.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
-
+import {IonSlides} from '@ionic/angular'
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
+
 export class Tab1Page {
 
-  constructor() {}
+  public imageLinks = [
+    "./assets/images/slide1.jpg",
+    "./assets/images/slide2.jpg",
+    "./assets/images/slide3.jpg",
+  ]
+
+  slideOptions = {
+    initialSlide: 0,
+    speed: 400,
+    autoplay: true,
+    loop: true
+  };
+
+  constructor() { 
+    console.log("Home tab created")
+    console.log(this.imageLinks)
+  }
+
+  public slidesDidLoad(slides: IonSlides) {
+    slides.startAutoplay();
+  }
 
 }
