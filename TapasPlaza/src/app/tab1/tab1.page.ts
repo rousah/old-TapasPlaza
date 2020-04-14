c6209d7b978b8c40fa0bbb5f757cbbd6ce22c6bd
Emilia Rosa van der Heide <emvader@epsg.upv.es>
2020-04-14 22:34:26 +0200

    food photos

--- TapasPlaza/src/app/tab1/tab1.page.ts
+++ TapasPlaza/src/app/tab1/tab1.page.ts
@@ -20,18 +20,26 @@ export class Tab1Page {
     "./assets/images/slide3.jpg",
   ]
 
-  foodImages = [["./assets/images/food1.jpg",
-    "Keep close to Nature's heart... and break clear away, once in awhile, and climb a mountain or spend a week in the woods. Wash your spirit clean.",
-    "Gambas PilPil"
+  foodImages = [["./assets/images/food/salads.JPG",
+    "Salads",
+    "A salad is a dish consisting of a mixture of small pieces of food, usually vegetables or fruit. The sauce used to flavor a salad is commonly called a salad dressing; most salad dressings are based on either a mixture of oil and vinegar or a fermented milk product like kefir.",
+    "Feta Salad"
   ],
-  ["./assets/images/food2.jpg",
-    "Keep close to Nature's heart... and break clear away, once in awhile, and climb a mountain or spend a week in the woods. Wash your spirit clean.",
-    "Gambas PilPil"
+  ["./assets/images/food/asian.JPG",
+    "Asian",
+    "Asian cuisine includes several major regional cuisines: Central Asian, East Asian, North Asian, South Asian, Southeast Asian, and West Asian. ... Ingredients common to many cultures in the East and Southeast regions of the continent include rice, ginger, garlic, sesame seeds, chilies, dried onions, soy, and tofu.",
+    "Nasi Goreng Special"
   ],
-  ["./assets/images/food3.jpg",
-    "Keep close to Nature's heart... and break clear away, once in awhile, and climb a mountain or spend a week in the woods. Wash your spirit clean.",
-    "Gambas PilPil"
-  ]
+  ["./assets/images/food/breakfast.JPG",
+    "Breakfast",
+    "The definition of breakfast is a morning meal, or the first meal of the day. Eggs and bacon that you eat first thing in the morning for your first meal is an example of breakfast.",
+    "Fresh OJ & homemade waffles"
+  ],
+  ["./assets/images/food/sweets.JPG",
+    "Sweets",
+    "Candy, also called sweets (British English) or lollies (Australian English, New Zealand English), is a confection that features sugar as a principal ingredient. The category, called sugar confectionery, encompasses any sweet confection, including chocolate, chewing gum, and sugar candy.",
+    "Homemade chocolate muffins"
+  ],
   ]
 
   public foodTexts = [
